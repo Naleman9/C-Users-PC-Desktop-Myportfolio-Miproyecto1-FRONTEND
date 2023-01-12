0b24170a7b7f877283093b4934c98a07f0e63056
pc <pc@gmail.com>
2023-01-12 12:34:52 -0300

    Se agrega el Proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaEducacionComponent } from './components/experiencia-educacion/experiencia-educacion.component';
+import { LogrosComponent } from './components/logros/logros.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    ExperienciaEducacionComponent,
+    LogrosComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    FooterComponent,
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
