f9deec577a4484537c03f028ba8c3f8d7773b6e0
Nereida Aleman <109105366+Naleman9@users.noreply.github.com>
2023-02-07 17:52:21 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,22 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { EncabezadoComponent } from './components/encabezado/encabezado.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
-import { ExperienciaEducacionComponent } from './components/experiencia-educacion/experiencia-educacion.component';
 import { LogrosComponent } from './components/logros/logros.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HabilidadesComponent } from './components/habilidades/habilidades.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { NewExperienciaComponent } from './new-experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './edit-experiencia/edit-experiencia.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { NeweducacionComponent } from './neweducacion/neweducacion.component';
+import { EditeducacionComponent } from './editeducacion/editeducacion.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+
 
 @NgModule({
   declarations: [
@@ -26,12 +36,23 @@ import { FooterComponent } from './components/footer/footer.component';
     HabilidadesComponent,
     ProyectosComponent,
     FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    NewExperienciaComponent,
+    EditExperienciaComponent,
+    EducacionComponent,
+    NeweducacionComponent,
+    EditeducacionComponent,
+    ExperienciaComponent,
+    
   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({})
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
